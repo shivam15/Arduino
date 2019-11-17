480c6542198964ebcaff206a63b7af159f6dc231
Shivam <shivam.sarawagi68@gmail.com>
2019-11-17 17:56:20 +0530

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  int i = 0;
+  for(i=0;i<5;i++) {
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+  }
+  for(i=0;i<5;i++) {
+  digitalWrite(13,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);
+  delay(2000);
+  }
+}
